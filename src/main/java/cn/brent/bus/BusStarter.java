017d430b9ca4d4d76220c50f5c302546e53a917f
covito <covito@163.com>
2015-08-21 16:22:55 +0800

    nn

--- src/main/java/cn/brent/bus/BusStarter.java
+++ src/main/java/cn/brent/bus/BusStarter.java
@@ -7,30 +7,36 @@ import cn.brent.bus.server.BusServer;
 import cn.brent.bus.util.Prop;
 
 public class BusStarter {
-	
-	public static final String CONFIG="bus-config.properties";
 
-	public static void main(String[] args) throws Exception {
-		String config=CONFIG;
-		if(args.length>0){
-			config=args[0];
+	public static final String CONFIG = "bus-config.properties";
+
+	public static void main(String[] args){
+		String config = null;
+		if (args.length > 0) {
+			config = args[0];
 		}
-		Prop pro=new Prop(config);
-		
-		int port=pro.getInt("port",15555);
-		int ioThreads=pro.getInt("ioThreads",1);
-		String registerToken=pro.get("registerToken");
-		boolean startMonitor=pro.getBoolean("startMonitor",true);
-		int monitorPort=pro.getInt("monitorPort",15556);
-		
-		BusServer server=new BusServer(ZMQ.context(ioThreads), port, registerToken);
+		start(config);
+	}
+
+	public static void start(String config){
+		if (config == null) {
+			config = CONFIG;
+		}
+		Prop pro = new Prop(config);
+
+		int port = pro.getInt("port", 15555);
+		int ioThreads = pro.getInt("ioThreads", 1);
+		String registerToken = pro.get("registerToken");
+		boolean startMonitor = pro.getBoolean("startMonitor", true);
+		int monitorPort = pro.getInt("monitorPort", 15556);
+
+		BusServer server = new BusServer(ZMQ.context(ioThreads), port, registerToken);
 		server.start();
-		
-		if(startMonitor){
-			MonitorServer ms=new MonitorServer(port, monitorPort);
+
+		if (startMonitor) {
+			MonitorServer ms = new MonitorServer(port, monitorPort);
 			ms.start();
 		}
 	}
-	
 
 }
